05c0cb91c13d58825de89ff53b08514edc3a712f
fahri-r <00.fahri.r@gmail.com>
2024-01-03 07:57:42 +0700

    fix styling for the untold story

--- pages/the-untold-story/[pageId]/index.tsx
+++ pages/the-untold-story/[pageId]/index.tsx
@@ -7,22 +7,25 @@ import { AnimateEnter } from "@/components/utils/AnimateEnter";
 import { NextSeo } from "next-seo";
 import profile from "@/data/profile";
 import Title from "@/components/utils/Title";
+import { getPageTitle } from "notion-utils";
 
 interface TheUntoldStoryPageProps {
-  project: ExtendedRecordMap;
+  recordMap: ExtendedRecordMap;
+  title: string;
 }
 
 export default function TheUntoldStoryPage({
-  project,
+  recordMap,
+  title,
 }: TheUntoldStoryPageProps) {
   return (
     <>
       <NextSeo title={`The Untold Story - ${profile.name}`} />
       <AnimateEnter className="max-w-[854px] max-lg:py-8 lg:w-4/5 lg:pt-8">
-        <Title variant="title">The Untold Story</Title>
         <NotionRenderer
+          pageTitle={<Title variant="title">{title} - The Untold Story</Title>}
           components={{ nextImage: Image, nextLink: Link }}
-          recordMap={project}
+          recordMap={recordMap}
           fullPage={true}
           darkMode={true}
           disableHeader
@@ -33,15 +36,17 @@ export default function TheUntoldStoryPage({
 }
 
 export async function getServerSideProps(context: any) {
-  const slug = context.query.slug;
+  const pageId = context.query.pageId;
 
   const notion = new NotionAPI();
   try {
-    const project = await notion.getPage(slug);
+    const recordMap = await notion.getPage(pageId);
+    const title = getPageTitle(recordMap);
 
     return {
       props: {
-        project,
+        recordMap,
+        title,
       },
     };
   } catch (error) {
