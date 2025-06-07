d28e43a3514801c7182a2d2bd06f3b7bc71cab4f
fahri-r <00.fahri.r@gmail.com>
2025-06-07 17:45:31 +0700

    feat: Add blog page

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -2,4 +2,5 @@ import { config } from 'dotenv';
 config();
 
 export const NOTION_KEY = process.env.NOTION_KEY || '';
-export const PROJECT_DATABASE_ID = process.env.DATABASE_ID || '';
+export const PROJECT_DATABASE_ID = process.env.PROJECT_DATABASE_ID || '';
+export const BLOG_DATABASE_ID = process.env.BLOG_DATABASE_ID || '';
