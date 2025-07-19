3cc730dd7d352e55818a8f380a9af4a9b0094063
fahri-r <00.fahri.r@gmail.com>
2025-07-19 11:20:13 +0700

    feat: Upgrade astro to 5.12.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
-import vercelStatic from '@astrojs/vercel/static';
-
 import sitemap from '@astrojs/sitemap';
-
 import mdx from '@astrojs/mdx';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.fahri-r.com',
-	integrations: [svelte(), sitemap(), mdx()],
-	vite: {
-		plugins: [tailwindcss()]
-	},
-	output: 'static',
-	adapter: vercelStatic()
+    site: 'https://www.fahri-r.com',
+    integrations: [svelte(), sitemap(), mdx()],
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    output: 'static',
+    adapter: cloudflare()
 });
