b7a3b9d4c23a6a9f158e91d9083e04d8b0fb1a38
fahri-r <00.fahri.r@gmail.com>
2025-07-21 18:39:50 +0700

    fix: Fix generate mdx command and fix blog const

--- src/libs/notion/databases/blog.database.ts
+++ src/libs/notion/databases/blog.database.ts
@@ -1,7 +1,7 @@
-import { PROJECT_DATABASE_ID } from '~/constants/global';
+import { BLOG_DATABASE_ID } from '~/constants/global';
 import type { DatabaseColumn } from '~/interfaces/notion/database-column.interface';
 
-export const DATABASE_ID = PROJECT_DATABASE_ID;
+export const DATABASE_ID = BLOG_DATABASE_ID;
 
 export const DATABASE: DatabaseColumn[] = [
 	{
