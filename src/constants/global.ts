f6f19dbb6e2341605f90d449fd87c987fb5eab3d
fahri-r <00.fahri.r@gmail.com>
2025-07-23 22:18:29 +0700

    fix: Change page title and description

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -7,11 +7,11 @@ export const BLOG_DATABASE_ID = process.env.BLOG_DATABASE_ID || '';
 
 export const SITE = {
 	website: 'https://fahri-r.com/', // replace this with your deployed domain
-	author: 'Muhammad Fahri Ramadhan',
+	author: 'Fahri Ramadhan',
 	profile: 'https://fahri-r.com/',
-	desc: 'Personal Website',
-	title: 'Muhammad Fahri Ramadhan',
-	ogImage: 'astropaper-og.jpg',
+	desc: 'Building scalable solutions and sharing tech insights. Explore my code, tutorials, and thoughts on modern software development.',
+	title: 'Fahri Ramadhan - Personal Website',
+	ogImage: 'og-image.jpeg',
 	dir: 'ltr', // "rtl" | "auto"
 	lang: 'en', // html lang code. Set this empty and default will be "en"
 	timezone: 'Asia/Jakarta' // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
