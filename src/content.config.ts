902a067e5a68ac686261858e87cf1352161185b9
fahri-r <00.fahri.r@gmail.com>
2025-12-15 22:52:35 +0700

    fix: Show mdx content

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,19 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const projects = defineCollection({
-	loader: glob({ pattern: '**/*.mdx', base: './src/content/projects' })
+	loader: glob({ pattern: '**/*.mdx', base: './src/content/projects' }),
+	schema: ({ image }) =>
+		z.object({
+			cover: image().nullable(),
+			title: z.string(),
+			description: z.string().nullable(),
+			date: z.coerce.date(),
+			repository: z.string().nullable(),
+			site: z.string().nullable(),
+			tools: z.array(z.string()),
+			category: z.string(),
+		})
 });
 
 export const collections = {
