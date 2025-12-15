e006cf1f4a19c7483e16071cb27f0b92b8398844
fahri-r <00.fahri.r@gmail.com>
2025-12-15 21:24:37 +0700

    feat: Remove notion converter

--- src/interfaces/notion/database-column.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface DatabaseColumn {
-	type: 'number' | 'select' | 'multi_select' | 'date' | 'url' | 'title' | 'rich_text';
-	columnName: string;
-	isRequired: boolean;
-	isFilename?: boolean;
-}
--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -4,14 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export const cn = (...inputs: ClassValue[]) => {
 	return twMerge(clsx(inputs));
 };
-
-export const pathJoin = (path: string, subPath: string) => {
-	return (
-		'/' +
-		path
-			.split('/')
-			.concat(subPath.split('/'))
-			.filter((p) => p)
-			.join('/')
-	);
-};
