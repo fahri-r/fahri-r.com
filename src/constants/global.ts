c02b73e62e7dfdb049626775edc6447013b49133
fahri-r <00.fahri.r@gmail.com>
2025-12-15 21:26:32 +0700

    fix: Remove unused const

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,10 +1,3 @@
-import { config } from 'dotenv';
-config();
-
-export const NOTION_KEY = process.env.NOTION_KEY || '';
-export const PROJECT_DATABASE_ID = process.env.PROJECT_DATABASE_ID || '';
-export const BLOG_DATABASE_ID = process.env.BLOG_DATABASE_ID || '';
-
 export const SITE = {
 	website: 'https://fahri-r.com/', // replace this with your deployed domain
 	author: 'Fahri Ramadhan',
