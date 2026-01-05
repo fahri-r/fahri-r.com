cea1e33e57892c15e0db41c22d14efd9ceed399c
fahri-r <00.fahri.r@gmail.com>
2026-01-06 00:26:49 +0700

    fix: Workaround for MessageChannel errror

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,15 @@ export default defineConfig({
 		})
 	],
 	vite: {
-		plugins: [tailwindcss()]
+		plugins: [tailwindcss()],
+		resolve: {
+			// https://github.com/withastro/astro/issues/12824
+			// Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+			// Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+			alias: import.meta.env.PROD && {
+				'react-dom/server': 'react-dom/server.edge'
+			}
+		}
 	},
 	output: 'static',
 	adapter: cloudflare()
