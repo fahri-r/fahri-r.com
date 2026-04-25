c941702cdfb84d4b0948e23892c414418c9dd8aa
fahri-r <00.fahri.r@gmail.com>
2026-04-26 00:26:21 +0700

    chore: bump astro version to 6.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,14 +21,6 @@ export default defineConfig({
 	],
 	vite: {
 		plugins: [tailwindcss()],
-		resolve: {
-			// https://github.com/withastro/astro/issues/12824
-			// Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-			// Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-			alias: import.meta.env.PROD && {
-				'react-dom/server': 'react-dom/server.edge'
-			}
-		}
 	},
 	output: 'static',
 	adapter: cloudflare()
